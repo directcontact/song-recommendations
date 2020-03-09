eba7775e116d8b4bdbb6996f631db192ee7ee0a3
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2020-03-09 01:44:50 -0400

    major revisions and changes, moved to using spotifyApi, bugs in express redirect

--- app.js
+++ app.js
@@ -1,149 +1,17 @@
 const express = require('express');
-const request = require('request');
 const cors = require('cors');
-const querystring = require('querystring');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const cryptoRandomString = require('crypto-random-string');
-
-require('dotenv').config();
-
-const client_id = process.env.CLIENT_ID;
-const client_id_secret = process.env.CLIENT_ID_SECRET;
-const redirect_uri = 'http://localhost:8080/callback';
-
+const path = require('path');
+const routes = require('./routes');
 const app = express();
 
-const stateKey = 'spotify_auth_state';
-
 app
-  .use(express.static(__dirname + '/public', { extensions: ['html'] }))
+  .use(express.static(__dirname + '/views'))
   .use(cors())
   .use(bodyParser.json())
-  .use(cookieParser());
-
-app.get('/api/v1/login', (req, res) => {
-  const scopes = 'user-read-private user-read-email';
-  const state = cryptoRandomString({ length: 16, type: 'base64' });
-  res.cookie(stateKey, state);
-  res.redirect(
-    'https://accounts.spotify.com/authorize?' +
-      querystring.stringify({
-        response_type: 'code',
-        client_id: client_id,
-        scope: scopes,
-        redirect_uri: redirect_uri,
-        state: state
-      })
-  );
-});
-
-// Redirect from callback to another url.
-app.get('/callback', (req, res) => {
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
-
-  if (state === null || state !== storedState) {
-    res.redirect(
-      '/#' +
-        querystring.stringify({
-          error: 'state_mismatch'
-        })
-    );
-  } else {
-    res.clearCookie(stateKey);
-
-    const authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        Authorization:
-          'Basic ' +
-          new Buffer(client_id + ':' + client_id_secret).toString('base64')
-      },
-      json: true
-    };
-
-    request.post(authOptions, (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        const refresh_token = body.refresh_token;
-        const access_token = body.access_token;
-
-        setAccessToken(access_token);
-        setRefreshToken(refresh_token);
-
-        const options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { Authorization: 'Bearer ' + access_token },
-          json: true
-        };
-
-        request.get(options, (error, response, body) => {
-          process.env.USER_ID = body.id;
-        });
-
-        res.send(body);
-      } else {
-        res.redirect(
-          '/#' +
-            querystring.stringify({
-              error: 'invalid_token'
-            })
-        );
-      }
-    });
-  }
-});
-
-app.get('/api/v1/token', function(req, res) {
-  // requesting access token from refresh token
-  const refresh_token = req.query.refresh_token;
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      Authorization:
-        'Basic ' +
-        new Buffer(client_id + ':' + client_id_secret).toString('base64')
-    },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      res.send({
-        access_token: access_token
-      });
-    }
-  });
-});
-
-app.get('/api/v1/playlists', (req, res) => {
-  const options = {
-    url: `https://api.spotify.com/v1/users/${process.env.USER_ID}/playlists`,
-    headers: { Authorization: 'Bearer ' + process.env.ACCESS_TOKEN },
-    json: true
-  };
-
-  request.get(options, (error, response, body) => {
-    console.log(body);
-
-    res.send(body);
-  });
-});
-
-const setAccessToken = access_token =>
-  (process.env.ACCESS_TOKEN = access_token);
-const setRefreshToken = refresh_token =>
-  (process.env.REFRESH_TOKEN = refresh_token);
+  .use(cookieParser())
+  .use('/', routes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}!`);
---
+++ routes/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const SpotifyWebApi = require('spotify-web-api-node');
+const cryptoRandomString = require('crypto-random-string');
+require('dotenv').config();
+
+const scopes = [
+  'user-read-private',
+  'user-read-email',
+  'playlist-modify-public',
+  'playlist-modify-private'
+];
+
+const state = cryptoRandomString({ length: 10, type: 'base64' });
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_ID_SECRET,
+  redirectUri: process.env.CALLBACK_URL
+});
+
+router.get('/', (req, res) => {
+  console.log('Request for home receieved');
+  res.render('index');
+});
+
+router.get('/login', (req, res) => {
+  const auth = spotifyApi.createAuthorizeURL(scopes, state);
+  try {
+    res.redirect(auth);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get('/callback', (req, res) => {
+  console.log('helko');
+  const { code } = req.query;
+  spotifyApi.authorizationCodeGrant(code).then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+    spotifyApi.setRefreshToken(data.body['refresh_token']);
+  });
+
+  res.redirect('http://localhost:8080/');
+});
+
+router.get('/refresh', (req, res) => {
+  spotifyApi.refreshAccessToken().then(data => {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  });
+});
+
+router.get('/playlists', (req, res) => {
+  console.log('Request for playlists received');
+  spotifyApi.getUserPlaylists().then(data => {
+    let playlists = data.body.items.map(item => item.name);
+    console.log(playlists);
+    res.send({ playlists });
+  });
+});
+
+module.exports = router;
