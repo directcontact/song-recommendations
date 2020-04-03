dbd13a5bfa67ec5fe59991e31a4da910c42a8766
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2020-04-03 12:50:43 -0400

    Removing footer

--- components/Layout.js
+++ components/Layout.js
@@ -3,15 +3,12 @@ import Header from './Header';
 
 const Layout = props => {
   return (
-    <div>
+    <section className="spotify">
       <Header />
-      <header className="masthead">
-        <div className="container h-100">{props.children}</div>
-      </header>
-      <div className="container">
-        <footer className="footer">&copy; 2020 - kimjeff49</footer>
+      <div className="spotify--container">
+        <div className="container justify-content-center">{props.children}</div>
       </div>
-    </div>
+    </section>
   );
 };
 
