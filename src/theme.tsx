249582ef5a13b044e56f660c443709db1332e122
Jeff Kim <42388571+kimjeff49@users.noreply.github.com>
2022-03-06 15:43:43 -0500

    [feat] Adding in some colors and more reworking

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,21 +1,29 @@
-import { extendTheme } from '@chakra-ui/react'
-import { createBreakpoints } from '@chakra-ui/theme-tools'
+import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 
-const fonts = { mono: `'Menlo', monospace` }
+const fonts = { mono: `'Menlo', monospace` };
 
 const breakpoints = createBreakpoints({
   sm: '40em',
   md: '52em',
   lg: '64em',
   xl: '80em',
-})
+});
 
 const theme = extendTheme({
   colors: {
-    black: '#16161D',
+    green: {
+      600: '#116e32',
+      500: '#1db954',
+      400: '#1ed760',
+    },
+    black: {
+      500: '#121212',
+      400: '#222326',
+    },
   },
   fonts,
   breakpoints,
-})
+});
 
-export default theme
+export default theme;
