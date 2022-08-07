a5749d9e7157bcb78bf88c9407dd0f5a3880ff6e
kimjeff49 <jeffkkim612@gmail.com>
2022-08-07 01:11:00 -0400

    [feat] adding in second page and also adding in some more styling + button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Input, VStack, Button } from "@chakra-ui/react";
 
 const Index = () => (
   <Flex
@@ -33,10 +33,14 @@ const Index = () => (
       position="absolute"
     ></Flex>
 
+    <Flex position="absolute" left="70px" top={200}>
+      <Heading color="white">/spotify-recommendations</Heading>
+    </Flex>
+
     <Flex
       h={80}
       w={250}
-      p={16}
+      p={6}
       bg="white"
       borderRadius="md"
       alignItems="flex-start"
@@ -45,9 +49,33 @@ const Index = () => (
       left={150}
       top={300}
     >
-      <Heading fontSize="xl" color="black">
-        Login
-      </Heading>
+      <VStack spacing={3}>
+        <Heading fontSize="2xl" color="black" mb={6} mt={10}>
+          login.
+        </Heading>
+
+        <Input placeholder="username" focusBorderColor="green" />
+        <Input placeholder="password" focusBorderColor="green" />
+        <Button
+          mt={3}
+          bg="white"
+          borderColor="green"
+          borderStyle="solid"
+          borderWidth="1px"
+          _hover={{
+            bg: "black",
+            borderWidth: "0",
+            color: "white",
+          }}
+          _active={{
+            bg: "white",
+            borderWidth: "1px",
+            color: "black",
+          }}
+        >
+          done
+        </Button>
+      </VStack>
     </Flex>
   </Flex>
 );
---
+++ src/pages/playlists.tsx
@@ -0,0 +1,5 @@
+import { Flex } from "@chakra-ui/react";
+
+export const Playlists = () => {
+  return <Flex></Flex>;
+};
