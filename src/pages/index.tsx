be0d83c3b2c0d1c3bf37fe8f96fb0423d43c2901
kimjeff49 <jeffkkim612@gmail.com>
2022-08-07 00:55:23 -0400

    [feat] new rework

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,56 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
-
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+import { Flex, Text, Heading } from "@chakra-ui/react";
+import Image from "next/image";
 
 const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text>
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+  <Flex
+    alignItems="center"
+    justifyContent="center"
+    h="1080px"
+    w="1920px"
+    position="relative"
+  >
+    <Flex
+      h="full"
+      w="full"
+      alignItems="center"
+      justifyContent="center"
+      position="absolute"
+    >
+      <video autoPlay muted loop style={{ height: "1080px", width: "1920px" }}>
+        <source src="/images/bg.mp4" />
+      </video>
+    </Flex>
 
-      <List spacing={3} my={0}>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+    <Flex
+      bgImage="/images/wave-haikei.svg"
+      aspectRatio="960/300"
+      backgroundRepeat="no-repeat"
+      backgroundPosition="center"
+      backgroundSize="cover"
+      h="full"
+      w="full"
+      alignItems="center"
+      justifyContent="center"
+      position="absolute"
+    ></Flex>
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+    <Flex
+      h={80}
+      w={250}
+      p={16}
+      bg="white"
+      borderRadius="md"
+      alignItems="flex-start"
+      justifyContent="center"
+      position="absolute"
+      left={150}
+      top={300}
+    >
+      <Heading fontSize="xl" color="black">
+        Login
+      </Heading>
+    </Flex>
+  </Flex>
+);
 
-export default Index
+export default Index;
